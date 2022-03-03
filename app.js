1b8cb1639c1d56bb99513d5cce00c013c1c552c7
Hugues Mourice <mourice.hugues@gmail.com>
2022-03-03 21:39:21 +0100

    added initArray & resetMenu

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
+// new array of products
 
+function initArrayProduct() {
+    var nbaJersey = ["Kevin Durant", "Lebron James", "Joel Embiid", "Demar DeRozan", "Luka Doncic", "Trae young", "Stephen Curry", "Giannis Antetokounmpo", "Nikola Jokic", "Ja Morant"];
+}
+
+// Cleaning ul/li list
+
+var ul = document.querySelector("ul");
+
+function resetMenu(){
+    var liValue = document.querySelectorAll('li');
+    for( const i of liValue ){
+        ul.removeChild(i);
+    }
+}
 
